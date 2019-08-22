c88b6d831ed4ef7fde04ccd75bb6ae861335e0aa
ajay1706 <ajay97456@gmail.com>
2019-08-22 22:19:11 +0530

    ai interact code with solution to value errors raised while user input

--- errors_project/ai_interact.py
+++ errors_project/ai_interact.py
@@ -1,12 +1,17 @@
 def interact():
     while True:
-        user_input = int(input("enter a value: "))
-        isEven = user_input %2 == 0
-        print('{} is {}'.format(user_input, 'even' if isEven else 'odd'))
+        try:
+            user_input = int(input("enter a value: "))
+        except ValueError:
+            print('PLease type integer only')
+        else:
+            print('{} is {}'.format(user_input, 'even' if user_input % 2 == 0 else 'odd'))
+        finally:
+            user_input = input('Do you want to play again? (Y/N): ')
 
-        user_input = input('Do you want to play again (Y/N)? ')
-
-        if user_input!= 'y':
+        if user_input != 'y':
             print('Good bye')
             break
+
+
 print(interact())
